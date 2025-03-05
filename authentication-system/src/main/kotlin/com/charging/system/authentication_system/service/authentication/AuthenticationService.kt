d25bb696cbff00c103253ada28d322fca93047d6
Brunda M Bharadwaj <brundabharadwaj22@gmail.com>
2025-03-05 14:49:21 +0530

    cleanup

--- authentication-system/src/main/kotlin/com/charging/system/authentication_system/service/authentication/AuthenticationService.kt
+++ authentication-system/src/main/kotlin/com/charging/system/authentication_system/service/authentication/AuthenticationService.kt
@@ -1,7 +1,8 @@
 package com.charging.system.authentication_system.service.authentication
 
+import com.charging.system.authentication_system.entities.AuthenticationEntity
 import java.util.UUID
 
 interface AuthenticationService {
-    fun findByStationIdAndDriverId(stationId:UUID,driverId:UUID)
+    fun findByStationIdAndDriverId(stationId:UUID,driverId:UUID): AuthenticationEntity?
 }
--- authentication-system/src/main/kotlin/com/charging/system/authentication_system/service/authentication/AuthenticationServiceImpl.kt
+++ authentication-system/src/main/kotlin/com/charging/system/authentication_system/service/authentication/AuthenticationServiceImpl.kt
@@ -1,12 +1,13 @@
 package com.charging.system.authentication_system.service.authentication
 
+import com.charging.system.authentication_system.entities.AuthenticationEntity
 import com.charging.system.authentication_system.repositories.AuthenticationRepository
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
 class AuthenticationServiceImpl (private val authenticationRepository: AuthenticationRepository): AuthenticationService{
-    override fun findByStationIdAndDriverId(stationId: UUID, driverId: UUID) {
-        authenticationRepository.findByStationUuidAndDriverId(stationId,driverId)
+    override fun findByStationIdAndDriverId(stationId: UUID, driverId: UUID) : AuthenticationEntity?{
+       return authenticationRepository.findByStationUuidAndDriverId(stationId,driverId)
     }
 }
